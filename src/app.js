1f713bdb56e417b88c64d702817c8fa76abfcd85
leonardo rosales <leonardo.rosalesa2@gmail.com>
2023-10-02 14:06:04 -0400

    modificacion 7

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: true })); // sino se agrega no podremos t
 
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:6000");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5000");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
