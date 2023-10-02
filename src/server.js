4d51294ac1e4002b7bfc5d702fc4e848e99a0a92
leonardo rosales <leonardo.rosalesa2@gmail.com>
2023-10-02 09:58:24 -0400

    modificacion 3

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ const options = {
 };
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:6000");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
